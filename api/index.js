935dbe70bb070a47643bcbe2e72aff594dc4e476
john-fuentes <149728976+john-fuentes@users.noreply.github.com>
2024-07-30 01:41:02 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials:true,
-    origin: 'http://localhost:5173',
+    origin: 'http://127.0.0.1:5173',
 }));
 
 
